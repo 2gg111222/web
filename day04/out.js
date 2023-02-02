c440ab233fcc64b30f7c7182709042d908cf0dbd
2gg111222 <124110995+2gg111222@users.noreply.github.com>
2023-02-02 17:52:40 +0900

    Add files via upload

---
+++ day04/out.js
@@ -0,0 +1,20 @@
+// 함수는 여러절차가 있는 처리를 이름을 부여하여 기능을 정의!
+
+function 빨래하다() {
+    console.log("1. 먼지를 털자.")
+    console.log("2. 물에 집어 넣는다.")
+    console.log("3. 비누를 바른다.")
+    console.log("4. 오물조물 비빈다.")
+    console.log("5. 물에 여러번 헹군다.")
+    console.log("6. 짠다.")
+    console.log("7. 넌다.")
+ }
+ 
+  function 청소하다() {
+    console.log("1. 환기를 시킨다.")
+    console.log("2. 물건을 정리한다.")
+    console.log("3. 청소기를 돌린다.")
+    console.log("4. 물걸레를 한다.")
+    console.log("5. 정리한다.")
+    console.log("6. 창문을 닫는다." )
+  }
---
+++ day04/out2.js
@@ -0,0 +1,17 @@
+// 함수는 여러절차가 있는 처리를 이름을 부여하여 기능을 정의!
+
+function 여행하다() {
+    console.log("1. 여행비용을 모은다.")
+    console.log("2. 여행지를 고른다.")
+    console.log("3. 여행계획을 짠다.")
+    console.log("4. 여행을 간다.")
+    console.log("5. 여행을 즐긴다.")
+    console.log("6. 여행을 끝맞치다.")
+    console.log("7. 집으로 돌아와 휴식을 취한다.")
+ }
+ 
+  function 쉬다() {
+    console.log("1. 유튜브를 본다.")
+    console.log("2. 웹툰을 본다.")
+    console.log("3. 잠을 잔다.")
+  }
